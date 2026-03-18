316e6b9100d6c10ddcf156eeebe5ffa5a3937fe9
lootboxer <>
2026-03-18 23:08:39 +0300

    tokyonight-day theme

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -19,7 +19,7 @@ return {
 			untracked = { text = "┆" },
 		},
 		signs_staged_enable = true,
-		signcolumn = false, -- Toggle with :Gitsigns toggle_signs
+		signcolumn = true, -- Toggle with :Gitsigns toggle_signs
 		numhl = false, -- Toggle with :Gitsigns toggle_numhl
 		linehl = false, -- Toggle with :Gitsigns toggle_linehl
 		word_diff = false, -- Toggle with :Gitsigns toggle_word_diff
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -5,6 +5,6 @@ return {
     require('tokyonight').setup {
       styles = { comments = { italic = false } },
     }
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'tokyonight-day'
   end,
 }
