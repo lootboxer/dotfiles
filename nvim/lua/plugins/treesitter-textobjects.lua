631fdc4f8aeee104dd73b8f2ddbeb8acbb55692a
lootboxer <>
2026-03-18 22:21:52 +0300

    textobject fix

--- nvim/lua/plugins/treesitter-textobjects.lua
+++ nvim/lua/plugins/treesitter-textobjects.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter.config").setup({
 				textobjects = {
 					select = {
 						enable = true,
